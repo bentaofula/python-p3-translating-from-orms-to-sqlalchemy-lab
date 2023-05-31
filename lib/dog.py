b0bfc86dfc97cd7b985f3c3e2430e6c1e2187041
Oscar Okumu <ookumu@songa.app>
2023-06-01 00:10:26 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,33 @@
 from models import Dog
+from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
 
-def create_table(base):
-    pass
 
+def create_table(base, engine):
+   # engine= create_engine('sqlite:///dogs.db')
+    base.metadata.create_all(engine)
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    dogs=session.query(Dog).all()
+    return dogs
 
 def find_by_name(session, name):
-    pass
+    dog = session.query(Dog).filter_by(name=name).first()
+    return dog
 
 def find_by_id(session, id):
-    pass
+    dog = session.query(Dog).filter_by(id=id).first()
+    return dog
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter_by(name=name, breed=breed).first()
+    return dog
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed=breed
+    session.commit()
+    # session.close()
